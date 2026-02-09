f3a9855be20ab8fedde97a0222ac065dcde0331b
VISHNU S <140037992+vishnu7979@users.noreply.github.com>
2026-02-09 20:56:59 +0530

    load real events

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
-import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    // const response = await fetch(`${BASE_URL}/api/events`);
-    // const { events  } = await response.json(); 
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events  } = await response.json(); 
 
+ 
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
